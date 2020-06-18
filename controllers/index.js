16fbe495a9e2444c68264b858f03aad76236bdd2
winoooops <winoooops@outlook.com>
2020-06-18 15:48:43 +1000

    threejs doees not support SSR, so use the html/js combo still...

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,5 @@
 const express = require('express')
 const router = express.Router() 
-const THREE = require('three')
-const orbitControls = require("three-orbitcontrols")
-// const detector = require('detector-webgl')
-const stats = require('stats')
-const astar = require('astar')
-
-const Colors = require('../assets/colors.json')
-
-//dependent functions
-const initScene = () => {
-    // create the scene 
-    scene = new THREE.Scene() 
-    // add FOV fog effecct
-    scene.fog = new THREE.Fog(Colors.fog, 100, 950)
-}
-
-
-
-
-
-
-
 
 
 router.get('/', (req,res) => {
